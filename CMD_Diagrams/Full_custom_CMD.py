069e17da84cfb4f33fd13686b85e59f9ae8baa0d
ArturP2002 <145135267+ArturP2002@users.noreply.github.com>
2024-05-03 22:39:20 +0300

    Add files via upload

---
+++ CMD_Diagrams/Full_custom_CMD.py
@@ -0,0 +1,40 @@
+import re  # Библиотека для работы с регулярными выражениями
+import matplotlib.pyplot as plt  # Библиотека для работы с диаграммой
+
+
+def full_area():
+    """
+    Функция построения полной CMD из выбранных областей галактики
+    """
+    # Отбираем хорошие звезды из выбранных областей галактики с помощью эллипса и дуг юбочек
+    with open('good_stars.txt', 'r', encoding='UTF-8') as good:
+        with open('good_stars_final.txt', 'w', encoding='UTF-8') as final_file:
+            cmd_x_fin = []  # список для записи всех значений cmd по x
+            F814_FIN = []  # список для записи всех значений фильтра F814
+            while line := good.readline():
+                new_line = line.rstrip()
+                numbers = re.findall(r'-?\d+\.?\d*', new_line)
+                if float(numbers[9]) < 0.5 and float(numbers[10]) <= 2.0 and float(numbers[21]) >= 5.0 and float(
+                        numbers[22]) ** 2 <= 0.01 and float(numbers[24]) < 0.5 and float(numbers[25]) <= 2.0 and float(
+                    numbers[36]) >= 5.0 and float(numbers[37]) ** 2 <= 0.01 and float(numbers[39]) < 0.5 and float(
+                    numbers[40]) <= 2.0 and 1480.0 <= float(numbers[2]) <= 2027.0 or 1840.0 <= float(
+                    numbers[2]) <= 2578.0 or 2409.0 <= float(numbers[2]) <= 3014.0 or 3519.0 <= float(
+                    numbers[3]) <= 4153.0 or 2458.0 <= float(numbers[3]) <= 3519.00 or 1918.0 <= float(
+                    numbers[3]) <= 2596.0:
+                    cmd_x_fin.append(float(numbers[17]))
+                    F814_FIN.append(float(numbers[32]))
+                    final_file.write(f'{new_line} \n')
+
+    F606_FIN = []  # список для записи всех значений фильтра F606
+
+    for i_elem in range(len(cmd_x_fin)):
+        cmd_x_end2 = cmd_x_fin[i_elem] - F814_FIN[i_elem]
+        F606_FIN.append(cmd_x_end2)
+
+    data3 = list(zip(F606_FIN, F814_FIN))  # Создаем кортеж значений двух фильтров для CMD
+    x_coord3, y_coord3 = zip(*data3)
+    plt.scatter(x_coord3, y_coord3, s=0.2)
+    plt.xlabel('F606-F814')
+    plt.ylabel('F814')
+    plt.gca().invert_yaxis()
+    plt.show()
---
+++ CMD_Diagrams/Low_youbochka.py
@@ -0,0 +1,38 @@
+import re  # Библиотека для работы с регулярными выражениями
+import matplotlib.pyplot as plt  # Библиотека для работы с диаграммой
+
+
+def low_area():
+    """
+    Функция для записи в файл всех хороших звезд из нижней юбочки, построения CMD нижней юбочки
+    """
+    # Отбираем хорошие звезды нижней юбочки
+    with open('good_stars.txt', 'r', encoding='UTF-8') as good:
+        with open('good_stars_low.txt', 'w', encoding='UTF-8') as final_file:
+            cmd_x_fin = []  # список для записи всех значений cmd по x
+            F814_FIN = []  # список для записи всех значений фильтра F814
+            while line := good.readline():
+                new_line = line.rstrip()
+                numbers = re.findall(r'-?\d+\.?\d*', new_line)
+                if float(numbers[9]) < 0.5 and float(numbers[10]) <= 2.0 and float(numbers[21]) >= 5.0 and float(
+                        numbers[22]) ** 2 <= 0.01 and float(numbers[24]) < 0.5 and float(numbers[25]) <= 2.0 and float(
+                    numbers[36]) >= 5.0 and float(numbers[37]) ** 2 <= 0.01 and float(numbers[39]) < 0.5 and float(
+                    numbers[40]) <= 2.0 and 1480.0 <= float(numbers[2]) <= 2027.0 and 3519.0 <= float(
+                    numbers[3]) <= 4153.0:
+                    cmd_x_fin.append(float(numbers[17]))
+                    F814_FIN.append(float(numbers[32]))
+                    final_file.write(f'{new_line} \n')
+
+    F606_FIN = []  # список для записи всех значений фильтра F606
+
+    for i_elem in range(len(cmd_x_fin)):
+        cmd_x_end2 = cmd_x_fin[i_elem] - F814_FIN[i_elem]
+        F606_FIN.append(cmd_x_end2)
+
+    data3 = list(zip(F606_FIN, F814_FIN))  # Создаем кортеж значений двух фильтров для CMD
+    x_coord3, y_coord3 = zip(*data3)
+    plt.scatter(x_coord3, y_coord3, s=0.2)
+    plt.xlabel('F606-F814')
+    plt.ylabel('F814')
+    plt.gca().invert_yaxis()
+    plt.show()
